734aa7479bb55469b5fa61ce90e33054bccf4ad7
Devin <devin.18@cse.mrt.ac.lk>
2021-01-24 22:13:43 +0530

    db config changes

--- config/db.js
+++ config/db.js
@@ -1,6 +1,14 @@
 const postgres = require('postgres');
 
-const sql = postgres(process.env.DATABASE_URL);
-
+//const sql = postgres(process.env.DATABASE_URL);
+const sql = postgres({
+    host        : 'ec2-52-205-61-60.compute-1.amazonaws.com',         // Postgres ip address or domain name
+    port        : 5432,       // Postgres server port
+    database    : 'denllfns60eujm',         // Name of database to connect to
+    username    : 'zrhibfgouvlzlx',         // Username of database user
+    password    : '83e21b29a78cd048957a671ed11b1e17f9d00ada4478a1c51d5a6052eaae1be4',         // Password of database user
+    ssl         : true,      // True, or options for tls.connect
+    max         : 10,
+});
 
 module.exports = sql;
